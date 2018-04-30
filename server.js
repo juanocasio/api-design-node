722ad73e6ca541522e3f985c662cedc29204b789
juanocasio <juanocasio@comcast.net>
2018-04-29 21:03:06 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+const express = require('express');
+const path = require ('path');
+const app = express();
+const jsonData = {count: 12, message: 'hey'};
+const port = 8080;
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-var jsonData = {count: 12, message: 'hey'};
+app.get('/data', function(req, res){
+  res.json(jsonData);
+});
+
+app.listen (port, function() {
+  console.log('Listening on port: ', port);
+});
